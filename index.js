04841f7b2b74e9b3b1e04245685f7afe02cea995
Neto <j.altevirneto@gmail.com>
2024-08-06 11:07:47 -0300

    feat: importando codigo no main code

---
+++ index.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+const corretora = require('./modulos/corretora.js');
+const corretor = require('./modulos/corretor.js');
+const clientes = require('./modulos/cliente.js');
